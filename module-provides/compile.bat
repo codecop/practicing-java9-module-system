ae3547809b38fab9fe674f489d3ea3a92da32fe3
Peter Kofler <peter.kofler@code-cop.org>
2020-12-07 22:00:27 +0100

    Provide imcomplete compile and run scripts.

---
+++ module-provides/compile.bat
@@ -0,0 +1,3 @@
+javac printer-api\src\module-info.java    printer-api\src\com\printer\api\Printer.java             -d dist\printer-api
+javac printer-impl\src\module-info.java   printer-impl\src\com\printer\impl\ConsolePrinter.java    -d dist\printer-impl
+javac printer-client\src\module-info.java printer-client\src\com\printer\client\PrinterClient.java -d dist\printer-client
